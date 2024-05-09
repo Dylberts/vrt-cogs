02fdd9de0f544775abe36f8c93f88d5f637f7c47
Dylberts <dylan_e_davis@hotmail.com>
2024-05-09 15:36:19 -0700

    Update views.py

--- tickets/common/views.py
+++ tickets/common/views.py
@@ -172,7 +172,7 @@ class CloseView(View):
             return
 
         conf = await self.config.guild(interaction.guild).all()
-        if not await can_close(self.bot, interaction.guild, interaction.channel, user, self.owner_id, conf):
+        if not await can_close(self.bot, interaction.guild, interaction.channel, user, self.owner_id, user_can_close, conf): #test
             return await interaction.response.send_message(
                 _("You do not have permissions to close this ticket"), ephemeral=True
             )
